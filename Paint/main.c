2ba355ce43b4f6832da0a36d52dcad36b765fb7b
Kelthon <kelthonbalbino@gmail.com>
2023-05-01 10:50:54 -0300

    updated

--- Paint/main.c
+++ Paint/main.c
@@ -1,47 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <windows.h>
-#include <GL/glut.h>
-#include <locale.h>
-#include "function.h"
+#include <gl/glut.h>
 
+#include "buffer.h"
+
+#define WINDOW_WIDTH 600.0
+#define WINDOW_HEIGTH 600.0
+#define WINDOW_POSITION_X 650.0
+#define WINDOW_POSITION_Y 0.0
 
 void init(void)
 {
-    glClearColor(1.0, 1.0, 1.0, 0.0); //cor da tela
-
-    glMatrixMode(GL_PROJECTION); //matriz de posi��o
-    glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0); //proje��o ortogonal
-
+    glClearColor(1.0, 1.0, 1.0, 0.0);
+    glMatrixMode(GL_PROJECTION);
+    gluOrtho2D(0, WINDOW_WIDTH, WINDOW_HEIGTH, 0);
 }
 
 void display(void)
 {
-    glClear(GL_COLOR_BUFFER_BIT); //desenha o fundo e limpa a janela
+    glClear(GL_COLOR_BUFFER_BIT);
 
-    designPoint();
-    designLine();
-    designPoligon();
+    buffer_init();
+    buffer_draw_all();
 
     glutSwapBuffers();
-
-
 }
-
 
 int main(int argc, char** argv)
 {
-    setlocale(LC_ALL, "Portuguese");
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowSize(window_width, window_height);
-    glutInitWindowPosition(200, 0);
-    glutCreateWindow("Paint");
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGTH);
+    glutInitWindowPosition(WINDOW_POSITION_X, WINDOW_POSITION_Y);
+    glutCreateWindow("Paint by L&K");
     glutMouseFunc(mouse);
-    createMenu();
+    create_menu();
 
     init();
     glutDisplayFunc(display);
     glutMainLoop();
-
 
     return 0;
 }
