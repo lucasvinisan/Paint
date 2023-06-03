ef0ef4c3a3f5e5a7b592038906a9bb61a271db1d
Kelthon <kelthonbalbino@gmail.com>
2023-06-03 14:32:01 -0300

    fixed rotation teapot

--- Teapot/main.c
+++ Teapot/main.c
@@ -8,63 +8,106 @@
 #define WINDOW_POSITION_X 550.0
 #define WINDOW_POSITION_Y 0.0
 
+double angle = 0;
+int rotate = 1;
+
 void init() {
-    glClearColor(0, 0, 0, 0);
+    glLoadIdentity();
     glEnable(GL_DEPTH_TEST);
     glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
+    glClearColor(.5, .5, .5, .5);
     glOrtho(-WINDOW_WIDTH, WINDOW_WIDTH, -WINDOW_HEIGTH, WINDOW_HEIGTH, WINDOW_DEPTH, -WINDOW_DEPTH);
+    lighting();
+}
+
+void lighting() {
+    float global_light[3] = {.2f, .2f, .2f};
+    float ligth0_position[] = {0, 175, -1000, 1};
+    float ligth0_ambient[] = {0, 0, 0, 1};
+    float ligth0_diffuse[] = {1, 1, 1, 1};
+    float ligth0_specular[] = {1, 1, 1, 1};
+
+    float material_diffuse[] = {1.f, .5f, 0.f, 1};
+    float material_specular[] = {.9f, .9f, .9f, 1};
+    float material_shininess = 70;
+
+    glLightfv(GL_LIGHT0, GL_POSITION, ligth0_position);
+    glLightfv(GL_LIGHT0, GL_AMBIENT, ligth0_ambient);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, ligth0_diffuse);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, ligth0_specular);
+
+    glLightModelfv(GL_LIGHT_MODEL_AMBIENT, global_light);
+
+    glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, material_diffuse);
+    glMaterialfv(GL_FRONT, GL_SPECULAR, material_specular);
+    glMaterialf(GL_FRONT, GL_SHININESS, material_shininess);
+
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+}
+
+void Text2D(char *text, int length, int font, int x, int y) {
+
+}
+
+void animation(int value) {
+    glutPostRedisplay();
+    glutTimerFunc(16, animation, 0);
+
+    angle += 0.005;
+
+    if(angle >= 360) angle = 0;
+
+    glPushMatrix();
+        if(rotate) glRotatef(angle, 0, 1, 0.1);
+
+        else {
+
+        }
+
+        glutSolidTeapot(250.f);
+    glPopMatrix();
+}
+
+void mouse(int button, int state, int x, int y) {
+
+    switch(button) {
+        case GLUT_MIDDLE_BUTTON:
+            if(state == GLUT_DOWN)
+                rotate = !rotate;
+
+            angle = 0;
+            glLoadIdentity();
+            break;
+
+        default:
+            break;
+    }
 }
 
 void display() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
 
-    glEnable(GL_LIGHTING);
-    glEnable(GL_COLOR_MATERIAL);
+    glViewport(0, WINDOW_HEIGTH / 2, WINDOW_WIDTH / 2, WINDOW_HEIGTH / 2);
+    gluLookAt(0, 0, 10, 0, 0, 0, 0, 1, 0);
+    animation(0);
+    glLoadIdentity();
+    glViewport(WINDOW_WIDTH / 2, WINDOW_HEIGTH / 2, WINDOW_WIDTH / 2, WINDOW_HEIGTH / 2);
+    gluLookAt(-10, 0, 0, 0, 0, 0, 0, 1, 0);
+    animation(0);
+    glLoadIdentity();
 
-    float global_light[] = {.2f, .2f, .2f, .2f};
-
-    glLightModelfv(GL_LIGHT_MODEL_AMBIENT, global_light);
-
-    float sun[4][4] = {
-        {0.1f, 0.1f, 0.1f, 1.0f},
-        {0.8f, 0.8f, 0.8f, 1.0f},
-        {1.0f, 1.0f, 1.0f, 1.0f},
-        {100.0f, 110.0f, -110.0f, 1.0f}
-    };
-
-    glLightfv(GL_LIGHT0, GL_AMBIENT, &sun[0][0]);
-    glLightfv(GL_LIGHT0, GL_DIFFUSE, &sun[1][0]);
-    glLightfv(GL_LIGHT0, GL_SPECULAR, &sun[2][0]);
-    glLightfv(GL_LIGHT0, GL_POSITION, &sun[3][0]);
-
-    glEnable(GL_LIGHT0);
-
-//    glLoadIdentity();
-//    gluLookAt(-50, -100, 200, 0, 0, 0, 0, 1, 0);
-//    glViewport(0, 0, WINDOW_WIDTH / 2, WINDOW_HEIGTH / 2);
-    glColor3f(.1f, .5f, 1.f);
+    glViewport(0, 0, WINDOW_WIDTH / 2, WINDOW_HEIGTH / 2);
+    gluLookAt(0, -10, 0, 0, 0, 0, 0, 0, 1);
+    animation(0);
+    glLoadIdentity();
+    glViewport(WINDOW_WIDTH / 2, 0, WINDOW_WIDTH / 2, WINDOW_HEIGTH / 2);
     gluLookAt(-50, -100, 200, 0, 0, 0, 0, 1, 0);
-    glutSolidTeapot(100.f);
-
-//    glLoadIdentity();
-//    glViewport(WINDOW_WIDTH / 2, 0, WINDOW_WIDTH / 2, WINDOW_HEIGTH / 2);
-//    gluLookAt(-50, -100, 200, 0, 0, 0, 0, 1, 0);
-//    glColor3f(.5f, .5f, 1.f);
-//    glutSolidTeapot(100.f);
-//
-//        glLoadIdentity();
-//    gluLookAt(-50, -100, 200, 0, 0, 0, 0, 1, 0);
-//    glViewport(0, WINDOW_HEIGTH / 2, WINDOW_WIDTH / 2, WINDOW_HEIGTH / 2);
-//    glColor3f(.5f, .1f, .1f);
-//    glutSolidTeapot(100.f);
-//
-//    glLoadIdentity();
-//    glViewport(WINDOW_WIDTH / 2, WINDOW_HEIGTH / 2, WINDOW_WIDTH / 2, WINDOW_HEIGTH / 2);
-//    gluLookAt(-50, -100, 200, 0, 0, 0, 0, 1, 0);
-//    glColor3f(.5f, .0f, 1.f);
-//    glutSolidTeapot(100.f);
+    gluPerspective(10, 0.5, 300, 300);
+    animation(0);
+    glLoadIdentity();
 
     glutSwapBuffers();
 }
@@ -79,8 +122,9 @@ int main(int argc, char **argv) {
     init();
 
     glutDisplayFunc(display);
+    glutMouseFunc(mouse);
+    glutTimerFunc(0, animation, 0);
     glutMainLoop();
-
 
     return 0;
 }
